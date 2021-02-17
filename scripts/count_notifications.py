860aacb2ade6a120beb8883aac438e9c3b2c24f2
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-02-17 18:28:59 +0100

    Add count_notifications.py

---
+++ scripts/count_notifications.py
@@ -0,0 +1,21 @@
+"""
+Count the number of notifications sent within a period of time,
+and the number of recipients who read them.
+"""
+
+from pontoon.base.models import *
+from notifications.models import Notification
+from datetime import datetime
+
+notifications_h2_2020 = Notification.objects.filter(
+    timestamp__gte=datetime(2020, 7, 1),
+    timestamp__lte=datetime(2020, 12, 31),
+)
+
+unread_notifications_h2_2020 = notifications_h2_2020.filter(unread=False)
+
+notifications_h2_2020.count()
+unread_notifications_h2_2020.count()
+
+notifications_h2_2020.values_list("recipient").distinct().count()
+unread_notifications_h2_2020.values_list("recipient").distinct().count()
