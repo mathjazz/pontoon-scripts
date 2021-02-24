345f0066322ce73a6bcc52b8aac077b8911754d2
Matjaž Horvat <matjaz.horvat@gmail.com>
2021-02-24 15:58:14 +0100

    Rename variables

--- scripts/count_notifications.py
+++ scripts/count_notifications.py
@@ -12,10 +12,10 @@ notifications_h2_2020 = Notification.objects.filter(
     timestamp__lte=datetime(2020, 12, 31),
 )
 
-unread_notifications_h2_2020 = notifications_h2_2020.filter(unread=False)
+read_notifications_h2_2020 = notifications_h2_2020.filter(unread=False)
 
 notifications_h2_2020.count()
-unread_notifications_h2_2020.count()
+read_notifications_h2_2020.count()
 
 notifications_h2_2020.values_list("recipient").distinct().count()
-unread_notifications_h2_2020.values_list("recipient").distinct().count()
+read_notifications_h2_2020.values_list("recipient").distinct().count()
