ed91cbbad35e8d3c89ca9d20d6024b16e5c69914
Francesco Lodolo <flod@lodolo.net>
2023-07-05 11:15:51 +0200

    Add script for pretranslation PROD (from July 1st)

---
+++ stats/pretranslation/pretranslation_evaluation_prod.py
@@ -0,0 +1,93 @@
+"""
+Retrieve a list of pretranslated strings and print its metadata.
+
+This is used in the beta phase, with strings only scored via algorithm.
+
+Output is formatted as CSV with the following columns:
+
+* Project slug
+* Locale code
+* String URL
+* Translation time
+* Review time
+* Hours to review
+* Status
+* chrF++ Score
+
+Run the script in Pontoon's Django shell, e.g.:
+heroku run --app mozilla-pontoon ./manage.py shell
+"""
+
+import math
+from datetime import datetime
+from django.utils.timezone import get_current_timezone
+from pontoon.base.models import *
+from sacrebleu.metrics import CHRF
+
+chrfpp = CHRF(word_order=2)
+
+pt_users = User.objects.filter(
+    email__in=[
+        "pontoon-tm@example.com",
+        "pontoon-gt@example.com",
+    ]
+)
+
+START_DATE = "01/07/2023"
+tz = get_current_timezone()
+start_date = tz.localize(datetime.strptime(START_DATE, "%d/%m/%Y"))
+
+pretranslations = (
+    Translation.objects.filter(user__in=pt_users, date__gte=start_date)
+    .filter(Q(approved=True) | Q(rejected=True))
+    .prefetch_related("actionlog_set")
+    .order_by("entity__resource__project", "locale__code", "pk")
+)
+
+output = [
+    "Project,Locale,String,Translation time,Review time,Hours to review,Status,chrF++ Score"
+]
+errors = []
+for t in pretranslations:
+    entity = t.entity
+    resource = entity.resource
+    project = resource.project.slug
+    locale = t.locale.code
+    url = "https://pontoon.mozilla.org/{}/{}/{}?string={}".format(
+        locale,
+        project,
+        resource.path,
+        entity.pk,
+    )
+    translation_time = t.date
+    if t.approved:
+        action_type = "translation:approved"
+    else:
+        action_type = "translation:rejected"
+    review_time = t.actionlog_set.filter(action_type=action_type).first().created_at
+    time_to_review = (review_time - translation_time).total_seconds()
+    status = "approved" if t.approved else "rejected"
+    try:
+        score = chrfpp.sentence_score(t.string, [Translation.objects.get(entity=entity, approved=True, locale=t.locale).string])
+    except Translation.DoesNotExist:
+        errors.append(f"No approved translation available for: {url}.")
+        continue
+    comment = t.comments.first()
+    output.append(
+        '{},{},{},{},{},{},{},{}'.format(
+            project,
+            locale,
+            url,
+            translation_time.strftime("%d-%m-%Y %H:%M:%S"),
+            review_time.strftime("%d-%m-%Y %H:%M:%S"),
+            math.ceil(time_to_review / 3600),
+            status,
+            float(score.format(score_only=True)),
+        )
+    )
+
+if errors:
+    output.append(f"\n*** IGNORED STRINGS ({len(errors)}) ***")
+    output += errors
+
+print("\n".join(output))
