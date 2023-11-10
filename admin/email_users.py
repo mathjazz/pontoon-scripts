831fb0177e5dcf400f63bf18d683990db210a71a
Matjaž Horvat <matjaz.horvat@gmail.com>
2023-11-10 01:26:37 +0100

    Update sender in email_users.py

--- admin/email_users.py
+++ admin/email_users.py
@@ -16,26 +16,26 @@ Please find the feature spec and the prototype on Discourse:
 https://discourse.mozilla.org/t/request-for-feedback-locale-health-dashboard-for-managers/70197
 
 Thanks,
-L10n-drivers
+Mozilla L10n Team
 """
 
 html = u"""Hi,
 <br><br>
-We are working on Community Health Dashboards to help you and other<br>
-locale managers identify issues within the community related to<br>
+We are working on Community Health Dashboards to help you and other
+locale managers identify issues within the community related to
 translation and review activity.
 <br><br>
 Please find the feature spec and the prototype on <a href="https://discourse.mozilla.org/t/request-for-feedback-locale-health-dashboard-for-managers/70197">Discourse</a>.
 <br><br>
 Thanks,<br>
-L10n-drivers
+Mozilla L10n Team
 """
 
 for email in emails:
   msg = EmailMultiAlternatives(
     subject=subject,
     body=text,
-    from_email='Pontoon Team <pontoon-team@mozilla.com>',
+    from_email='Mozilla L10n Team <pontoon-team@mozilla.com>',
     # Do not put the entire list into the "to" field
     # or everyone will see all email addresses.
     to=[email]
