cc86874501bd7c21887d45bf0b96fb145772281a
Matjaž Horvat <matjaz.horvat@gmail.com>
2023-11-14 15:57:57 +0100

    Add Approved translations column to the covid general stats (#15)

--- stats/covid/general_stats.py
+++ stats/covid/general_stats.py
@@ -6,7 +6,8 @@ Output is formatted as CSV with the following columns:
 * Period (month, year)
 * Number of new user registered
 * Number of active users
-* Number of translations submitted
+* Number of all translations submitted (includes pretranslations and suggestions, excludes imported)
+* Number of approved translations submitted
 * Number of strings added
 
 Run the script in Pontoon's Django shell, e.g.:
@@ -79,12 +80,26 @@ for x in translations:
     period = "{}-{:02d}".format(x["period"].year, x["period"].month)
     if not period in data:
         data[period] = {}
-    data[period]["translations"] = x["count"]
+    data[period]["all_translations"] = x["count"]
+
+# Approved Translation Submissions
+translations = (
+    Translation.objects.filter(user__isnull=False, approved=True)
+    .annotate(period=TruncMonth("date"))
+    .values("period")
+    .annotate(count=Count("id"))
+    .order_by("period")
+)
+for x in translations:
+    period = "{}-{:02d}".format(x["period"].year, x["period"].month)
+    if not period in data:
+        data[period] = {}
+    data[period]["approved_translations"] = x["count"]
 
 # Generate output
 output = []
 output.append(
-    "Period,New User Registrations,Active Users,Translations Submitted,Strings Added"
+    "Period,New User Registrations,Active Users,All Translations Submitted,Approved Translations Submitted,Strings Added"
 )
 periods = list(data.keys())
 periods.sort()
@@ -94,14 +109,16 @@ for period in periods:
         period_data["registrations"] if "registrations" in period_data else 0
     )
     active = period_data["active"] if "active" in period_data else 0
-    translations = period_data["translations"] if "translations" in period_data else 0
+    all_translations = period_data["all_translations"] if "all_translations" in period_data else 0
+    approved_translations = period_data["approved_translations"] if "approved_translations" in period_data else 0
     added = period_data["added"] if "added" in period_data else 0
     output.append(
-        "{},{},{},{},{}".format(
+        "{},{},{},{},{},{}".format(
             period,
             registrations,
             active,
-            translations,
+            all_translations,
+            approved_translations,
             added,
         )
     )
